00466f0d6974ea89385dd9726129cfe8d0b3a39d
Kieran Schwegman <kschwegman1@gmail.com>
2024-10-01 22:29:15 -0400

    Added armor and combat component logic

--- Source/Sandbox/Characters/Components/Camera/CharacterCameraLogic.cpp
+++ Source/Sandbox/Characters/Components/Camera/CharacterCameraLogic.cpp
@@ -24,7 +24,7 @@ ACharacterCameraLogic::ACharacterCameraLogic(const FObjectInitializer& ObjectIni
 	CameraArm = CreateDefaultSubobject<UTargetLockSpringArm>(TEXT("Camera Arm"));
 	CameraArm->SetupAttachment(RootComponent); // Attach this to the mesh because if we attach this to the root, whenever we crouch the springArm/Camera will move along with it, which is not intended
 	CameraArm->TargetOffset = FVector(0, 0, 100); 
-	CameraArm->TargetArmLength = 340; // Distance from the character
+	CameraArm->TargetArmLength = 240; // Distance from the character
 	CameraArm->bUsePawnControlRotation = true; // Allows us to rotate the camera boom along with our controller when we're adding mouse input
 	CameraArm->ProbeSize = 16.4;
 	CameraArm->bEnableCameraLag = true;
@@ -38,7 +38,7 @@ ACharacterCameraLogic::ACharacterCameraLogic(const FObjectInitializer& ObjectIni
 	CameraStyle = CameraStyle_ThirdPerson;
 	CameraOrientation = ECameraOrientation::Center;
 
-	TargetArmLength = 340;
+	TargetArmLength = 240;
 	CameraLag = 2.3;
 	CameraOrientationTransitionSpeed = 3.4;
 	CameraOffset_FirstPerson = FVector(10.0, 0.0, 64);
